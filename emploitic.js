955d83edcaa562ab81cb1e9cbcc86e266e1fe5e4
BmTechT <146330858+BmTechT@users.noreply.github.com>
2024-05-14 15:19:35 +0100

    Update emploitic.js

--- emploitic.js
+++ emploitic.js
@@ -1,30 +1,23 @@
 const getJobs = async title => {
   const baseUrl = "https://emploitic.com/api/v4/jobs";
   const params = {
-    filter: "(criteria.location.id=%27c1dfd96eea8cc2b62785275bca38ac261256e278%27 OR criteria.location.metadata.parentId=%27c1dfd96eea8cc2b62785275bca38ac261256e278%27)",
+    filter: "(criteria.location.id='c1dfd96eea8cc2b62785275bca38ac261256e278' OR criteria.location.metadata.parentId='c1dfd96eea8cc2b62785275bca38ac261256e278')",
     q: title,
-    "pagination[page]": 2,
-    "pagination[pageSize]": 20,
+    "pagination[page]": 1,
+    "pagination[pageSize]": 1000,
     "sort[0]": "publishedAt:desc",
   };
 
   const queryString = new URLSearchParams(params).toString();
   const url = `${baseUrl}?${queryString}`;
-
   try {
-    const response = await fetch(url, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+    const response = await fetch(url);
 
     if (!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`);
     }
 
-    const data = await response.json();
-    return data;
+    return response.json();
   } catch (error) {
     console.error("Fetch error: ", error);
   }
